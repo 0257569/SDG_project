7f74243d0e06022f59b7116369862ee8b3832e72
eselemu <119244920+eselemu@users.noreply.github.com>
2023-10-25 19:42:05 -0600

    attempt of handling posts

--- server.js
+++ server.js
@@ -1,11 +1,53 @@
 const express = require('express');
 const bodyParser = require ("body-parser");
+const multer = require('multer');
 
 const app = express();
+const upload = multer({ dest: 'uploads/' });
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.engine("ejs", require("ejs").renderFile);
 app.set("view engine", "ejs");
+
+//All this is going to be obtained from the server
+const posts = [
+    {
+      author: 'User1',
+      topic: 'First Post',
+      content: 'This is the first post. Welcome to the community!',
+      image: 'image1.jpg',
+      comments: [
+        {
+          author: 'User2',
+          text: 'Welcome! Excited to be here.',
+          createdAt: new Date(), // Comment creation timestamp
+        },
+        {
+          author: 'User3',
+          text: 'Thanks for starting this community!',
+          createdAt: new Date(), // Comment creation timestamp
+        },
+      ],
+      createdAt: new Date(), // Post creation timestamp
+    },
+    {
+      author: 'User4',
+      topic: 'Second Post',
+      content: 'Here\'s the second post. Feel free to share your thoughts!',
+      image: 'image2.jpg',
+      comments: [
+        {
+          author: 'User5',
+          text: 'Looking forward to some interesting discussions!',
+          createdAt: new Date(), // Comment creation timestamp
+        },
+      ],
+      createdAt: new Date(), // Post creation timestamp
+    },
+  ];
+  
+
+var username = "eselemu";
 
 app.route("/")
 .get((req, res) =>{
@@ -22,7 +64,26 @@ app.route("/login")
 
 app.route("/forum")
 .get((req, res) =>{
-    res.render("forum");
+    res.render("forum", {posts: posts, username: username});
+});
+
+app.post('/post', upload.single('image'), (req, res) => {
+    const topic = req.body.topic;
+    const content = req.body.content;
+    //For now I wont be handling images or post it them through the server as this will be done from the database :)
+    const image = req.file;
+  
+    const post = {
+        author: username,
+        topic: topic,
+        content: content,
+        image: image.filename,
+        comments: [],
+        createdAt: new Date() 
+    };
+    console.log(post);
+    posts.push(post);
+    res.redirect('/forum');
 });
 
 app.route("/newsletter")
