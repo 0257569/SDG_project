e4d111fc0f8be54a86ce2e97c70f26ee6f504bff
eselemu <119244920+eselemu@users.noreply.github.com>
2023-10-30 18:14:34 -0600

    Beta v01 +Posts username

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(session({
   saveUninitialized: true
 })); 
 
-var username = "eselemu";
+var username = "";
 
 const userFilePath = "./data/user.json";
 const user = require(userFilePath);
@@ -85,14 +85,6 @@ app.route("/login")
     }
   });
 
-/*app.route("/login")
-.get((req, res) =>{
-  res.render("login_singup");
-}) 
-.post((req, res) => {
-  res.redirect("/");
-});*/
-
 // Serve images from the "uploads" directory
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 //Routes to my json files
@@ -101,7 +93,7 @@ const topicsFilePath = "./data/topics.json"
 //Get the list of posts and topics in my json
 const posts = require(postsFilePath);
 const topics = require(topicsFilePath);
-//Current topic of the posts section
+//Current and default topic of the posts section
 var currTopic = "HEALTH";
 
 // Configure Multer for image uploads
@@ -124,85 +116,102 @@ app.route("/forum")
   //Filter the posts depending on the topic
   filteredPosts = posts.filter(post => post.topic === currTopic);
   // Render your EJS template with the filtered posts
-  res.render("forum", { posts: filteredPosts, username: username, currTopic: currTopic, topics: topics.topics });
+  res.render("forum", { posts: filteredPosts, username: req.session.username, currTopic: currTopic, topics: topics.topics });
 });
 
 //My post endpoint that is triggered by my post action
 //It uploads the images that receives from the request(if any is uploaded)
 app.post('/post', upload.single('image'), (req, res) => {
-  //Recieve the topic and the content of the post
-  const topic = req.body.topic.toUpperCase();
-  const content = req.body.content;
-  //Creation of json object with the atributes of the post
-  const post = {
-    author: username,
-    topic: topic,
-    content: content,
-    image: req.file ? `/uploads/${req.file.filename}` : null,//Here if a file is received then it will save in the json object the route to teh alredy uploaded img in uploads
-    comments: [],//Comments array, by default the post doesnt have any
-    createdAt: moment(new Date()).format('MMM DD, YYYY, HH:mm:ss'),//Date of creation, it is formatted using moment (In order to look nicer and more readable)
-  };
+  //The user needs to be logged
+  if(req.session.username == undefined){
+    res.status(401).send("Not logged user");
+  }
+  else{
+    //Recieve the topic and the content of the post
+    const topic = req.body.topic.toUpperCase();
+    const content = req.body.content;
+    //Creation of json object with the atributes of the post
+    const post = {
+      author: req.session.username,
+      topic: topic,
+      content: content,
+      image: req.file ? `/uploads/${req.file.filename}` : null,//Here if a file is received then it will save in the json object the route to teh alredy uploaded img in uploads
+      comments: [],//Comments array, by default the post doesnt have any
+      createdAt: moment(new Date()).format('MMM DD, YYYY, HH:mm:ss'),//Date of creation, it is formatted using moment (In order to look nicer and more readable)
+    };
+  
+      posts.push(post); // Add the new post to the array
+      //If the topic doesnt exist then I add it
+      if(!(topics.topics.includes(topic))){
+        topics.topics.push(topic);
+      }
+      //Set the current topic to the topic of the new uploaded post
+      currTopic = topic;
+  
+      // Write the updated topics back to the JSON file
+      fs.writeFile(topicsFilePath, JSON.stringify(topics, null, 2), 'utf8', (err) => {
+        if (err) {
+          console.error('Error writing posts:', err);
+          return res.status(500).send('Server Error');
+        }
+      });
+      // Write the updated posts back to the JSON file
+      fs.writeFile(postsFilePath, JSON.stringify(posts, null, 2), 'utf8', (err) => {
+        if (err) {
+          console.error('Error writing posts:', err);
+          return res.status(500).send('Server Error');
+        }
+        //If no errors appear, then is redirected to forum end point
+        res.redirect('/forum');
+      });
+  }
+});
+//Endpoint to submit a comment to a post
+app.post('/postComment', (req, res) => {
+  //The user needs to be logged
+  if(req.session.username == undefined){
+    res.status(401).send("Not logged user");
+  }
+  else{
+    //Get the topic and the filtered posts from the post to be commented
+    currTopic = req.body.currTopic;
+    filteredPosts = posts.filter(post => post.topic === currTopic);
+    //Get the content of the comment and the index of the commented post
+    const content = req.body.postComment;
+    const indexPost = req.body.indexPost;
 
-    posts.push(post); // Add the new post to the array
-    //If the topic doesnt exist then I add it
-    if(!(topics.topics.includes(topic))){
-      topics.topics.push(topic);
-    }
-    //Set the current topic to the topic of the new uploaded post
-    currTopic = topic;
-
-    // Write the updated topics back to the JSON file
-    fs.writeFile(topicsFilePath, JSON.stringify(topics, null, 2), 'utf8', (err) => {
-      if (err) {
-        console.error('Error writing posts:', err);
-        return res.status(500).send('Server Error');
+    //Creation of the json object with the attributes of the comment
+    const comment = {
+      author: req.session.username,
+      text: content,
+      createdAt: moment(new Date()).format('MMM DD, YYYY, HH:mm:ss'), // Comment creation timestamp
+    };
+    //Set the comment in the original array of posts
+    posts.forEach(post => {
+      if(filteredPosts[indexPost] == post){
+        post.comments.push(comment);
       }
     });
-    // Write the updated posts back to the JSON file
+    //Write updated posts json file with added comment
     fs.writeFile(postsFilePath, JSON.stringify(posts, null, 2), 'utf8', (err) => {
       if (err) {
         console.error('Error writing posts:', err);
         return res.status(500).send('Server Error');
       }
-      //If no errors appear, then is redirected to forum end point
+  
       res.redirect('/forum');
     });
+  }
 });
-
-app.post('/postComment', (req, res) => {
-  currTopic = req.body.currTopic;
-  filteredPosts = posts.filter(post => post.topic === currTopic);
-  console.log(currTopic);
-  const content = req.body.postComment;
-  const indexPost = req.body.indexPost;
-  console.log(indexPost);
-
-  const comment = {
-    author: username,
-    text: content,
-    createdAt: moment(new Date()).format('MMM DD, YYYY, HH:mm:ss'), // Comment creation timestamp
-  };
-  posts.forEach(post => {
-    if(filteredPosts[indexPost] == post){
-      post.comments.push(comment);
-    }
-  });
-  fs.writeFile(postsFilePath, JSON.stringify(posts, null, 2), 'utf8', (err) => {
-    if (err) {
-      console.error('Error writing posts:', err);
-      return res.status(500).send('Server Error');
-    }
-
-    res.redirect('/forum');
-  });
-});
-
+//Enpoint to search topic
 app.route("/searchTopic")
 .get((req, res) =>{
   const topic = req.query.topic.toUpperCase();
+  //If the topic exists, we set the current topic to the searched one
   if(topics.topics.includes(topic)){
     currTopic = topic;
   }
+  //Else the default topic will be shown
   else{
     currTopic = "HEALTH"
   }
